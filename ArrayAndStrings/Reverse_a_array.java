3c11b6bb34cdddca242cf4d36c7ff710e1a67f29
Shiba Mishra <shibam@adobe.com>
2026-07-08 16:26:23 +0530

    Initial commit: DSA Java solutions

---
+++ ArrayAndStrings/Reverse_a_array.java
@@ -0,0 +1,39 @@
+package ArrayAndStrings;
+import java.util.*;
+
+public class Reverse_a_array {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        reverseArray(arr);
+        System.out.println("Reversed Array:");
+        printArray(arr);
+    }
+
+    static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    static void reverseArray(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
